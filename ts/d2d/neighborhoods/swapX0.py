1d9b95c9bac68bfedd49270b8c2f02db71b4a9cf
khoint5304 <sherlockholmes5304@gmail.com>
2023-11-03 10:19:57 +0700

    Fix SwapX0

--- ts/d2d/neighborhoods/swapX0.py
+++ ts/d2d/neighborhoods/swapX0.py
@@ -44,26 +44,12 @@ class Swappoint(D2DNeighborhoodMixin, _BaseNeighborhood):
 
         # swap Technician - Technician
 
-        for i, j in itertools.permutations(range(solution.technicians_count), 2):
+        bundles: List[IPCBundle[Swappoint, List[Tuple[int, int]]]] = [IPCBundle(self, []) for _ in range(pool_size)]
+        bundles_iter = itertools.cycle(bundles)
 
-            i_path = solution.technician_paths[i]
-            j_path = solution.technician_paths[j]
-
-            for point_i in range(1, len(i_path) - self.length):
-                for location_j in range(1, len(j_path) - 1):
-                    technician_path = list(solution.technician_paths)
-                    pi = list(technician_path[i])
-                    pj = list(technician_path[j])
-                    pj[location_j:location_j] = technician_path[i][point_i:point_i + self.length]
-                    pi[point_i:point_i + self.length] = []
-                    technician_path[i] = tuple(pi)
-                    technician_path[j] = tuple(pj)
-                    s = self.cls(
-                        drone_paths=solution.drone_paths,
-                        technician_paths=tuple(technician_path),
-                        drone_config_mapping=solution.drone_config_mapping,
-                    )
-                    s.add_to_pareto_set(results)
+        for pair in itertools.permutations(range(solution.technicians_count), 2):
+            x = next(bundles_iter)
+            x.data.append(pair)  # type: ignore
 
         return results
 
@@ -73,53 +59,47 @@ class Swappoint(D2DNeighborhoodMixin, _BaseNeighborhood):
         neighborhood.ensure_imported_data()
 
         solution = neighborhood._solution
-        first_length = neighborhood.length
-        second_length = neighborhood.length
-
-        # Don't alter the variables without a prefix underscore, edit their copies instead
-        technician_paths = list(list(path) for path in solution.technician_paths)
-
         results: Set[SolutionFactory] = set()
-        swaps_mapping: Dict[SolutionFactory, Tuple[int, int]] = {}
-        for first, second in bundle.data:
-            first_path = technician_paths[first]
-            second_path = technician_paths[second]
+        for i, j in bundle.data:
+            i_path = solution.technician_paths[i]
+            j_path = solution.technician_paths[j]
 
-            for first_start, second_start in itertools.product(
-                range(1, len(first_path) - first_length),
-                range(1, len(second_path) - second_length),
-            ):
-                _first_path = first_path.copy()
-                _second_path = second_path.copy()
+            for point_i in range(1, len(i_path) - neighborhood.length):
+                for location_j in range(1, len(j_path) - 1):
+                    pi = list(solution.technician_paths[i])
+                    pj = list(solution.technician_paths[j])
+                    pj[location_j:location_j] = solution.technician_paths[i][point_i:point_i + neighborhood.length]
+                    pi[point_i:point_i + neighborhood.length] = []
 
-                _first_path[first_start:first_start + first_length] = second_path[second_start:second_start + second_length]
-                _second_path[second_start:second_start + second_length] = first_path[first_start:first_start + first_length]
+                    first_arrival_timestamps = solution.calculate_technician_arrival_timestamps(pi)
+                    second_arrival_timestamps = solution.calculate_technician_arrival_timestamps(pj)
 
-                first_arrival_timestamps = solution.calculate_technician_arrival_timestamps(_first_path)
-                second_arrival_timestamps = solution.calculate_technician_arrival_timestamps(_second_path)
+                    _technician_timespans = list(solution.technician_timespans)
+                    _technician_timespans[i] = first_arrival_timestamps[-1]
+                    _technician_timespans[j] = second_arrival_timestamps[-1]
 
-                _technician_timespans = list(solution.technician_timespans)
-                _technician_timespans[first] = first_arrival_timestamps[-1]
-                _technician_timespans[second] = second_arrival_timestamps[-1]
+                    _technician_total_waiting_times = list(solution.technician_waiting_times)
+                    _technician_total_waiting_times[i] = solution.calculate_technician_total_waiting_time(pi, arrival_timestamps=first_arrival_timestamps)
+                    _technician_total_waiting_times[j] = solution.calculate_technician_total_waiting_time(pj, arrival_timestamps=second_arrival_timestamps)
 
-                _technician_total_waiting_times = list(solution.technician_waiting_times)
-                _technician_total_waiting_times[first] = solution.calculate_technician_total_waiting_time(_first_path, arrival_timestamps=first_arrival_timestamps)
-                _technician_total_waiting_times[second] = solution.calculate_technician_total_waiting_time(_second_path, arrival_timestamps=second_arrival_timestamps)
+                    operation_result = SolutionFactory(
+                        update_technicians=((i, tuple(pi)), (j, tuple(pj))),
+                        drone_timespans=solution.drone_timespans,
+                        drone_waiting_times=solution.drone_waiting_times,
+                        technician_timespans=tuple(_technician_timespans),
+                        technician_waiting_times=tuple(_technician_total_waiting_times),
+                    )
+                    operation_result.add_to_pareto_set(results)
+                    factory = SolutionFactory(
+                        update_technicians=((i, tuple(pi)), (j, tuple(pj))),
+                        drone_timespans=solution.drone_timespans,
+                        drone_waiting_times=solution.drone_waiting_times,
+                        technician_timespans=tuple(_technician_timespans),
+                        technician_waiting_times=tuple(_technician_total_waiting_times),
+                    )
 
-                _technician_paths = deepcopy(technician_paths)
-                _technician_paths[first] = _first_path
-                _technician_paths[second] = _second_path
-
-                operation_result = SolutionFactory(
-                    update_technicians=((first, tuple(_first_path)), (second, tuple(_second_path))),
-                    drone_timespans=solution.drone_timespans,
-                    drone_waiting_times=solution.drone_waiting_times,
-                    technician_timespans=tuple(_technician_timespans),
-                    technician_waiting_times=tuple(_technician_total_waiting_times),
-                )
-
-                pair = (first_path[first_start], second_path[second_start])
-                swaps_mapping[operation_result] = (min(pair), max(pair))
-                operation_result.add_to_pareto_set(results)
+                pair = (pi[first_start], pj[second_start])
+                swaps_mapping[factory] = (min(pair), max(pair))
+                factory.add_to_pareto_set(results)
 
         return set((r, swaps_mapping[r]) for r in results)
