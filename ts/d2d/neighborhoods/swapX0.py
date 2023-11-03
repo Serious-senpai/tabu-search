8ea61220faeb6df96a880a8685539734145b4719
khoint5304 <sherlockholmes5304@gmail.com>
2023-11-03 10:31:33 +0700

    Sorted imports and remove duplicates

--- ts/d2d/neighborhoods/swapX0.py
+++ ts/d2d/neighborhoods/swapX0.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 from multiprocessing import pool as p
 from typing import Iterable, Set, TYPE_CHECKING
-from copy import deepcopy
 from typing import Dict, Iterable, List, Set, Tuple, TYPE_CHECKING
 import itertools
-import functools
 from .mixins import D2DNeighborhoodMixin
 from .factory import SolutionFactory
-from ..config import DroneEnergyConsumptionMode
-from ..errors import NeighborhoodException
 from ...abc import MultiObjectiveNeighborhood
 from ...bundle import IPCBundle
-from .mixins import D2DNeighborhoodMixin
-from ...abc import MultiObjectiveNeighborhood
 if TYPE_CHECKING:
     from ..solutions import D2DPathSolution
 
