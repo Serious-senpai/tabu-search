f7d81db7e2042be1603cf88bf44d3bfa89617e48
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2023-11-07 13:16:14 +0700

    Fixed bug

--- ts/d2d/neighborhoods/mixins.py
+++ ts/d2d/neighborhoods/mixins.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TypeVar, TYPE_CHECKING
+from typing import Generic, TypeVar, TYPE_CHECKING
 
 from ...abc import MultiObjectiveNeighborhood
 if TYPE_CHECKING:
@@ -12,25 +12,27 @@ __all__ = ("D2DBaseNeighborhood",)
 
 _T = TypeVar("_T")
 if TYPE_CHECKING:
-    _BaseNeighborhood = MultiObjectiveNeighborhood[D2DPathSolution, _T]
+
+    class D2DBaseNeighborhood(MultiObjectiveNeighborhood[D2DPathSolution, _T]):
+        __slots__ = ()
+        def __init__(self, solution: D2DPathSolution, /) -> None: ...
+        def ensure_imported_data(self) -> None: ...
+
 else:
-    _BaseNeighborhood = MultiObjectiveNeighborhood
+    class D2DBaseNeighborhood(MultiObjectiveNeighborhood, Generic[_T]):
 
+        __slots__ = ()
 
-class D2DBaseNeighborhood(_BaseNeighborhood[_T]):
+        def __init__(self, solution: D2DPathSolution, /) -> None:
+            super().__init__(solution)  # type: ignore
+            self.extras["problem"] = solution.problem
+            self.extras["drone_config_mapping"] = solution.drone_config_mapping
+            self.extras["energy_mode"] = solution.energy_mode
 
-    __slots__ = ()
-
-    def __init__(self, solution: D2DPathSolution, /) -> None:
-        super().__init__(solution)  # type: ignore
-        self.extras["problem"] = solution.problem
-        self.extras["drone_config_mapping"] = solution.drone_config_mapping
-        self.extras["energy_mode"] = solution.energy_mode
-
-    def ensure_imported_data(self) -> None:
-        if self.cls.problem != self.extras["problem"]:
-            self.cls.import_problem(
-                self.extras["problem"],
-                drone_config_mapping=self.extras["drone_config_mapping"],
-                energy_mode=self.extras["energy_mode"],
-            )
+        def ensure_imported_data(self) -> None:
+            if self.cls.problem != self.extras["problem"]:
+                self.cls.import_problem(
+                    self.extras["problem"],
+                    drone_config_mapping=self.extras["drone_config_mapping"],
+                    energy_mode=self.extras["energy_mode"],
+                )
