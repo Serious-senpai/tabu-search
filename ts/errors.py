936e3e3d03855cc66ade4cf1e7eb7ce733b830cf
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2023-11-09 18:12:02 +0700

    Allow reseting tabu age

--- ts/errors.py
+++ ts/errors.py
@@ -1,13 +1,6 @@
-from typing import Any
+__all__ = ("TabuSearchException",)
 
 
 class TabuSearchException(Exception):
     """Base class for all exceptions from this library"""
     pass
-
-
-class AlreadyInTabu(TabuSearchException):
-    """Exception raised when an operation is already in the tabu list"""
-
-    def __init__(self, target: Any, /) -> None:
-        super().__init__(f"Already in tabu list: {target!r}")
