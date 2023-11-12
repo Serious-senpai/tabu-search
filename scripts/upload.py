3e6105a2c4e3b4e23b2a621873b0dda498b698cd
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2023-11-12 18:27:05 +0700

    Ensure valid Unix file name

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,13 +1,12 @@
 import os
+import re
 import sys
-from urllib.parse import quote
 from urllib.request import Request, urlopen
+
 
 token = os.environ["HARUKA_TOKEN"]
 path = sys.argv[1]
-name = quote(sys.argv[2])
-
-
+name = "".join(c for c in sys.argv[2] if re.match(r"[a-zA-Z0-9_\-()\.]", c) is not None)
 with open(path, "rb") as file:
     request = Request(
         url=f"https://haruka39.me/api/upload?name={name}&extract=1",
