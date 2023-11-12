a72161f87a622543f4112092da354c4876614e6d
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2023-11-12 12:46:51 +0700

    Fixed workflow

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,15 +1,16 @@
 import os
 import sys
+from urllib.parse import quote
 from urllib.request import Request, urlopen
-
 
 token = os.environ["HARUKA_TOKEN"]
 path = sys.argv[1]
+name = quote(sys.argv[2])
 
 
 with open(path, "rb") as file:
     request = Request(
-        url="https://haruka39.me/api/upload",
+        url=f"https://haruka39.me/api/upload?name={name}&extract=1",
         data=file.read(),
         headers={"X-Auth-Token": token},
         method="POST",
